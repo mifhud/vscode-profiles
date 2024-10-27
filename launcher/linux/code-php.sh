90071463e29a95d47c1c40ae24b97696f48f5f1c
Miftahul Huda <miftahul.huda@jatis.com>
2024-10-27 19:32:28 +0700

    feat: version 3

---
+++ launcher/linux/code-php.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if a directory parameter is provided
+if [ -z "$1" ]; then
+  echo "No directory provided. Opening home directory in WSL."
+  DIRECTORY="~"
+elif [ "$1" == "." ]; then
+  echo "Opening current directory in WSL."
+  DIRECTORY=$(pwd)
+else  
+  DIRECTORY="$1"
+fi
+
+"$VSCODE_DIR/code" --profile "PHP Lite" $DIRECTORY
---
+++ launcher/windows/code-php.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Check if a directory parameter is provided
+if "%~1"=="" (
+    echo No directory provided. Opening home directory.
+    set "DIRECTORY=~"
+) else if "%~1"=="." (
+    echo Opening current directory.
+    for /f "delims=" %%i in ('cd') do set "DIRECTORY=%%i"
+) else (
+    set "DIRECTORY=%~1"
+)
+
+REM Launch VSCode with the specified directory
+REM Adjust the profile and other parameters as necessary
+code --profile "PHP Lite" "%DIRECTORY%"
